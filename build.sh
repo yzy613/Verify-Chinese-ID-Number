5f65b0aafb958d22fac6277e26e9c3e8b03e636a
yzy613 <59520517+yzy613@users.noreply.github.com>
2020-02-08 10:46:32 +0800

    Update build

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w -s" -o Verify-Chinese-ID-Number ./main.go
-tar -czvf linux_amd64.tar.gz Verify-Chinese-ID-Number area_code.json
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w -s" -o verify-chinese-ID-number ./main.go
+tar -czvf linux_amd64.tar.gz verify-chinese-ID-number area_code.json
+rm -f verify-chinese-ID-number
 
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-w -s" -o Verify-Chinese-ID-Number.exe ./main.go
-tar -czvf windows_amd64.tar.gz Verify-Chinese-ID-Number.exe area_code.json
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-w -s" -o verify-chinese-ID-number.exe ./main.go
+tar -czvf windows_amd64.tar.gz verify-chinese-ID-number.exe area_code.json
+rm -f verify-chinese-ID-number.exe
